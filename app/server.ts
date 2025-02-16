bbfdc2f80acb6a62aab7e67a4f88387b76ed9b19
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2025-02-17 08:29:12 +0900

    observe and control server requests

--- app/server.ts
+++ app/server.ts
@@ -2,4 +2,10 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+app.use((c, next) => {
+  console.log("Incoming request url", c.req.url);
+  console.log("Incoming request user agent", c.req.header("User-Agent"));
+  return next();
+})
+
 export default app;
