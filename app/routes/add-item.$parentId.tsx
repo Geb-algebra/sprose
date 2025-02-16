7678254309557944ab47f2a36f6b85411dd4823c
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2025-02-16 14:08:08 +0900

    fix styles

--- app/routes/add-item.$parentId.tsx
+++ app/routes/add-item.$parentId.tsx
@@ -78,9 +78,9 @@ export function AddItemCardButton(props: {
 						setWriting(true);
 					}}
 					className={cn(
-						"w-48 h-16 mr-2 mb-2 grid place-content-center bg-transparent rounded-md transition-colors",
-						"border-2 border-dashed border-slate-100 hover:border-slate-400 focus-visible:border-slate-400",
-						"text-2xl text-slate-100 hover:text-slate-400 focus-visible:text-slate-400 ",
+						"w-48 h-16 mr-2 mb-2 grid place-content-center bg-transparent rounded-md transition-colors outline-none",
+						"border-2 border-dashed border-slate-200 hover:border-slate-400 focus-visible:border-slate-400",
+						"text-2xl text-slate-200 hover:text-slate-400 focus-visible:text-slate-400",
 					)}
 				>
 					+
