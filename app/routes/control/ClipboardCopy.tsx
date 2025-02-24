8ad90c17722223f8040babb6a4861f688e5aa500
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2025-02-24 11:00:22 +0900

    copy paste whole map with ctrl cv

--- app/routes/control/ClipboardCopy.tsx
+++ app/routes/control/ClipboardCopy.tsx
@@ -1,10 +1,13 @@
 import { ClipboardCopyIcon } from "lucide-react";
-import React from "react";
 import { TooltipButton } from "~/components/TooltipButton";
+import { useKeyboardShortcut } from "~/hooks/useKeyboardShortcut";
 import type { Item } from "~/map/models";
 import { copyItemToClipboard } from "~/map/services/clipboard.client";
 
 export default function ClipboardCopy(props: { map: Item }) {
+	useKeyboardShortcut(["ctrl+c", "meta+c"], () => {
+		copyItemToClipboard(props.map);
+	});
 	return (
 		<TooltipButton
 			type="button"
